0e867b4afffad4468034dee2986484987618c7b6
Jérémy <rcd.jeremy@gmail.com>
2021-12-28 18:20:41 +0100

    Modal and handler

--- main.js
+++ main.js
@@ -11,12 +11,15 @@ var form = document.getElementById('form');
 var input = document.getElementById('input_message');
 var form_pseudo = document.getElementById('form_pseudo');
 var input_pseudo = document.getElementById('input_pseudo');
+var modal = document.getElementById('modal');
 
-const socket = io("176.128.9.92:8008");
 const COLORS = [
     'blue', 'green', 'yellow', 'purple', 'orange', 'pink', 'indigo', 'teal', 'gray', 'brown', 'black', 'white'
 ]
 const USERS = [];
+let socket;
+let me;
+
 class message {
     constructor(cmd, value, user) {
         this.cmd = cmd;
@@ -27,32 +30,14 @@ class message {
 }
 
 class user {
-    constructor(name) {
+    constructor(name, color) {
         this.name = name;
         USERS.push(this);
-        this.color = COLORS[USERS.indexOf(this)];
+        this.color = color ? color : COLORS[USERS.indexOf(this)-1] ;
     }
 }
 
-let me = new user('Anonyme');
-
-socket.on("connect", () => {
-    console.log(socket.id); // x8WIv7-mJelg7on_ALbx
-
-    // System message
-    new_message({
-        cmd: "system",
-        value: "Connected to the server",
-        user: { name: "System", color: "red" }
-    });
-
-    let msg = new message("newUser", "", me);
-    send(msg);
-});
-
-socket.on("disconnect", () => {
-    console.log(socket.id); // undefined
-});
+let System = new user("System", 'red');
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
@@ -66,9 +51,33 @@ form.addEventListener('submit', function(e) {
 
 form_pseudo.addEventListener('submit', function(e) {
     e.preventDefault();
-    console.log('New pseudo:' + input_pseudo.value)
-    me.name = input_pseudo.value;
+    if (input_pseudo.value) {
+        console.log('New pseudo:' + input_pseudo.value)
+        me = new user(input_pseudo.value);
+        // Hide the modal
+        init();
+        draw_users();
+        modal.style.display = "none";
+    }
 });
+
+function init() {
+    socket = io("176.128.9.92:8008");
+    
+    if(!me) {
+        modal.style.display = "block";
+    }
+
+    if(!socket.connected) {
+        modal.style.display = "block";
+    }
+       
+    socket.on("connect", on_connect);     
+    
+    socket.on("disconnect", on_disconnect);
+
+    socket.on('chat message', on_msg);
+}
 
 function send(msg) {
     socket.emit('chat message', msg);
@@ -99,20 +108,68 @@ function new_user(u) {
     new_message({
         cmd: "system",
         value: `${u.name} joined to the server`,
-        user: { name: "System", color: "red" }
+        user: System
+    });
+    draw_users();
+}
+
+function draw_users() {
+    let users = document.getElementById('users');
+    console.log('User:',USERS)
+    users.innerHTML = '';
+    USERS.forEach(u => {
+        let item = document.createElement('li');
+        item.classList.add('user');
+        item.innerHTML = `<span class="text-${u.color}-500">${u.name}</span>`;
+        users.appendChild(item);
     });
 }
 
-socket.on('chat message', function(msg) {
+function on_connect() {
+    console.log("Connected to the server");
+    console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+    console.log(socket.connected); // true
+        
+    // System message
+    new_message({
+        cmd: "system",
+        value: "Connected to the server",
+        user: System
+    });
+
+    let msg = new message("newUser", me, System);
+    send(msg);
+}
+
+function on_disconnect() {
+    console.log("Disconnected from the server");
+    console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+
+    // System message
+    new_message({
+        cmd: "system",
+        value: "Disconnected from the server",
+        user: System
+    });
+}
+
+function on_msg(msg) {
     console.log('Message received:' + msg)
 
-    if (msg.cmd == 'newMessage') {
+    if (msg.user == System) {
+        on_sys_msg(msg);
+    }
+    else if (msg.cmd == 'newMessage') {
         new_message(msg);
-    } else if (msg.cmd == 'system') {
-        new_message(msg);
-    } else if (msg.cmd == 'newUser') {
-        new_user(msg.user);
     }
 
     window.scrollTo(0, document.body.scrollHeight);
-});
+}
+
+function on_sys_msg(msg) {
+    console.log('System message received:' + msg)
+
+    if (msg.cmd == 'newUser') {
+        new_user(msg.user);
+    }
+}
